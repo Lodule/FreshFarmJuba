f773b72232cff979903e0f529b50bfd1efbc24c7
Lodule <simonlodule18@gmail.com>
2026-03-16 01:14:03 +0300

    chore:professionalize repo with license,editorconfig,and mail resolution

--- src/main/java/com/example/freshfarmjuba/config/MailConfig.java
+++ src/main/java/com/example/freshfarmjuba/config/MailConfig.java
@@ -1,5 +1,6 @@
 package com.example.freshfarmjuba.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -10,19 +11,31 @@ import java.util.Properties;
 @Configuration
 public class MailConfig {
 
+    @Value("${spring.mail.host}")
+    private String host;
+
+    @Value("${spring.mail.port}")
+    private int port;
+
+    @Value("${spring.mail.username}")
+    private String username;
+
+    @Value("${spring.mail.password}")
+    private String password;
+
     @Bean
     public JavaMailSender javaMailSender() {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-        mailSender.setHost("smtp.gmail.com");
-        mailSender.setPort(587);
-        mailSender.setUsername("${spring.mail.username}");
-        mailSender.setPassword("${spring.mail.password}");
+        mailSender.setHost(host);
+        mailSender.setPort(port);
+        mailSender.setUsername(username);
+        mailSender.setPassword(password);
 
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.transport.protocol", "smtp");
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.debug", "true");
+        props.put("mail.debug", "false"); // Set to false for production
 
         return mailSender;
     }
